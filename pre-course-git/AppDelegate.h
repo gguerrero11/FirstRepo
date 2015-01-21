9a6d28429b4abf2d66f58aa92aba4397d4d4acb0
gguerrero11 <gguerrero11@gmail.com>
2015-01-20 16:14:48 -0800

    Initial Commit

---
+++ pre-course-git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pre-course-git
+//
+//  Created by Gabriel Guerrero on 1/20/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pre-course-git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pre-course-git
+//
+//  Created by Gabriel Guerrero on 1/20/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
